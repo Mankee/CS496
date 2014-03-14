34d19485a020952f2b0b166777e92cc1cc6f2e9b
Austin Dubina <austin.dubina@jivesoftware.com>
2014-03-14 14:02:35 -0700

    App now regugarly displays gps location within the app. now working on uploading gps locations to Google App Engine

---
+++ final/src/edu/oregonstate/mobilecloud/MyGeoLocation.java
@@ -0,0 +1,15 @@
+package edu.oregonstate.mobilecloud;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyGeoLocation extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
